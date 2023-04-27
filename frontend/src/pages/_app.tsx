423472f49bf2ba005538de16b0837e7a71147726
= <danielcf.vix@hotmail.com>
2023-04-27 12:54:04 -0300

    fix:frontend dashboard route

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,13 +5,13 @@ import { AuthProvider } from "@/contexts/authContext";
 import { RegisterProvider } from "@/contexts/registerContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={customTheme}>
-      {/* <RegisterProvider> */}
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-      {/* </RegisterProvider> */}
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider theme={customTheme}>
+			{/* <RegisterProvider> */}
+			<AuthProvider>
+				<Component {...pageProps} />
+			</AuthProvider>
+			{/* </RegisterProvider> */}
+		</ChakraProvider>
+	);
 }
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -6,58 +6,59 @@ import nookies from "nookies";
 import api from "@/services/api";
 
 interface IDashboardProps extends IUser {
-  contacts: IContact[];
+	contacts: IContact[];
 }
 
 const Dashboard = ({ name, contacts }: IDashboardProps) => {
-  return (
-    <>
-      <Header name={name} isLogged />
-      <div className="dashboard-container">
-        {contacts.map((contact) => (
-          <div className="contact" key={contact.id}>
-            Name: {contact.name} - Phone Number: {contact.phoneNumber}
-          </div>
-        ))}
-      </div>
-      <style jsx>{`
-        .dashboard-container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          margin: 20px;
-          padding: 20px;
-          border: 1px solid #ccc;
-          border-radius: 5px;
-        }
+	return (
+		<>
+			<Header name={name} isLogged />
+			<div className="dashboard-container">
+				{contacts.map((contact) => (
+					<div className="contact" key={contact.id}>
+						Name: {contact.name} - Phone Number:{" "}
+						{contact.phoneNumber}
+					</div>
+				))}
+			</div>
+			<style jsx>{`
+				.dashboard-container {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					justify-content: center;
+					margin: 20px;
+					padding: 20px;
+					border: 1px solid #ccc;
+					border-radius: 5px;
+				}
 
-        .contact {
-          margin-top: 10px;
-        }
-      `}</style>
-    </>
-  );
+				.contact {
+					margin-top: 10px;
+				}
+			`}</style>
+		</>
+	);
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = nookies.get(ctx);
-  const id = cookies["id"];
+	const cookies = nookies.get(ctx);
+	const id = cookies["id"];
 
-  if (!cookies["token"]) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-  const userContacts = await api.get(`/user/${id}`);
-  const contacts = userContacts.data;
+	if (!cookies["token"]) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+	const userContacts = await api.get(`/contact/${id}`);
+	const contacts = userContacts.data;
 
-  return {
-    props: { name: cookies["user"], contacts },
-  };
+	return {
+		props: { name: cookies["user"], contacts },
+	};
 };
 
 export default Dashboard;
